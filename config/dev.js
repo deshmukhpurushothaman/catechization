baca7e02ac2a0ac32ef857e9dfc507f3bad8d97e
Deshmukh P <deshmukhpurushothaman@gmail.com>
2022-01-03 19:16:44 +0530

    dev.js commited

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  MONOGOURI:
+    'mongodb+srv://Deshmukh:deshmukh123@cluster0.z2bcf.mongodb.net/Cluster0?retryWrites=true&w=majority',
+  jwtPrivateKey: 'catechization',
+  SENDGRID_API_KEY: 'abcd1234',
+  EMAIL: 'deshmukhpurushothaman@gmail.com',
+};
